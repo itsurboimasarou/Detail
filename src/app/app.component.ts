be47c7e41a5f44e0718066826fbe5f253fb59aa8
itsurboimasarou <medicspam6@gmail.com>
2024-08-09 22:32:46 +0700

    Assets changes
    * background blur (still buggy)
    * Show more comment (not working rn)
    * Button padding & spacing (work perfectly, without components rn)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {MatIcon, MatIconModule} from '@angular/material/icon';
 import {MatButton, MatButtonModule} from '@angular/material/button';
 import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
 import {MatInput, MatInputModule} from '@angular/material/input';
-import {NgOptimizedImage} from "@angular/common";
+import {NgClass, NgOptimizedImage, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -19,7 +19,9 @@ import {NgOptimizedImage} from "@angular/common";
     MatFormFieldModule,
     MatInput,
     MatInputModule,
-    NgOptimizedImage
+    NgOptimizedImage,
+    NgStyle,
+    NgClass
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
@@ -58,6 +60,79 @@ export class AppComponent {
       name: 'Jane Smith',
       comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
       date: '2021-Jan-01'
-    }
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
+    {
+      avatar: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      name: 'Jane Smith',
+      comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget odio sit amet nunc sollicitudin porta. Sed ac purus auctor, ultrices libero nec, luctus libero. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia. Donec nec enim auctor, lacinia purus nec, fermentum turpis. Sed nec libero sit amet libero ultricies lacinia.',
+      date: '2021-Jan-01'
+    },
   ];
+  displayedComments = 10;
+
+  imageUrl = "./public/images/image.png"
+  loadMoreComments() {
+    this.displayedComments += 10;
+  }
+
+  favoriteIcon = 'favorite_outlined';
+  shareIcon = 'ios_share_outlined';
+  bookmarkIcon = 'bookmark_outlined';
+  likes = 856;
+
+  toggleFavorite() {
+    this.favoriteIcon = this.favoriteIcon === 'favorite_outlined' ? 'favorite' : 'favorite_outlined';
+    this.likes += this.favoriteIcon === 'favorite' ? 1 : -1;
+  }
+
+  toggleShare() {
+    this.shareIcon = this.shareIcon === 'ios_share_outlined' ? 'ios_share' : 'ios_share_outlined';
+  }
+
+  toggleBookmark() {
+    this.bookmarkIcon = this.bookmarkIcon === 'bookmark_outlined' ? 'bookmark' : 'bookmark_outlined';
+  }
+
 }
